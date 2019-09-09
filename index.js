eb8561bb943c9ca4589ca32d3218a6728b8d7c72
Nate Tan <nktan93@gmail.com>
2019-09-09 19:57:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
-function nestedTarget(target, nested) {
-  return document.querySelector(`#${nested} .${target}`);
+function nestedTarget() {
+  return document.querySelector(#nested .target);
 }
