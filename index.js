ab11d2b0fbb862f84ea5fcad92ee2b2241a35347
Nate Tan <nktan93@gmail.com>
2019-09-09 20:28:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild() {
+  let node = document.getElementById( 'grand-node' )
+  let nextNode = node.children[ 0 ]
+
+  while ( nextNode ) {
+    node = nextNode
+    nextNode = node.children[ 0 ]
+  }
+
+  return node
+}
+
+function increaseRankBy(n) {
+  const lis = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
