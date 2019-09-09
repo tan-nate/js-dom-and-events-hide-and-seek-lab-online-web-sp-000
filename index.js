596fff026d1e9b7822a693d120daccf04740786f
Nate Tan <nktan93@gmail.com>
2019-09-09 20:28:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function increaseRankBy(n) {
     .querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML) + 3;
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
   }
 }
