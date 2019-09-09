a7ba3b2bd9ca6dd82a56cb0dafc21ff6ef7ca152
Nate Tan <nktan93@gmail.com>
2019-09-09 19:53:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
+
+function nestedTarget(target, nested) {
+  return document.getElementById(nested).getElementsByClassName(target);
+}
