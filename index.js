87f6dcba7aa9feb636a04d788c846de07952a8c4
Nate Tan <nktan93@gmail.com>
2019-09-09 20:26:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function increaseRankBy(n) {
   const lis = document
     .getElementById('app')
     .querySelectorAll('ul.ranked-list li');
-   
+
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt(lis[i].innerHTML) + 1;
   }
